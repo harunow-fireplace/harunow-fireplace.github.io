f8f6d9aab3bd2a5b9010754af51bf1f2629d261a
Debs <deborahkhoo36@gmail.com>
2024-07-30 18:04:23 +0800

    fix copy address

--- js/event.js
+++ js/event.js
@@ -68,44 +68,61 @@ $('.color-picker div').on('click', function () {
     $(this).addClass('selected');
 });
 
-$('#copy-address').on('click', function () {
-    // Get the text to copy
-    var textToCopy = $('#copy-address').text();
+// Handle click event for each element with the class 'copy-address'
+$('.copy-address').on('click', function () {
+    // Get the current clicked element
+    var $this = $(this);
+
+    // Get the text to copy from the clicked element
+    var textToCopy = $this.text().trim();
 
     // Use the Clipboard API to copy text
     navigator.clipboard.writeText(textToCopy).then(function () {
         // Success callback
-        var tooltip = bootstrap.Tooltip.getInstance('#copy-address'); // Get the tooltip instance
-        tooltip.setContent({ '.tooltip-inner': 'Copied!' }); // Update tooltip content
-        tooltip.show(); // Show the tooltip
+        var tooltip = bootstrap.Tooltip.getInstance($this[0]); // Get the tooltip instance for the clicked element
 
-        $('#copy-icon').html(`
+        if (tooltip) {
+            tooltip.setContent({ '.tooltip-inner': 'Copied!' }); // Update tooltip content
+            tooltip.show(); // Show the tooltip
+        }
+
+        // Update the icon within the clicked element
+        $this.find('#copy-icon').html(`
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-check-lg" viewBox="0 0 16 16">
                 <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
-            </svg>`);
+            </svg>
+        `);
 
+        // Change icon color to green
+        $this.css('color', '#6A8D4D');
+
+        // Revert changes after 2 seconds
         setTimeout(function () {
-            tooltip.hide(); // Hide the tooltip after 2 seconds
-            tooltip.setContent({ '.tooltip-inner': 'Copy' }); // Update tooltip content
+            if (tooltip) {
+                tooltip.hide(); // Hide the tooltip
+                tooltip.setContent({ '.tooltip-inner': 'Copy' }); // Reset tooltip content
+                tooltip.show(); // Show the tooltip again to reflect the change
+            }
 
-            $('#copy-icon').html(`
+            // Reset the icon back to the original
+            $this.find('#copy-icon').html(`
                 <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                         fill="currentColor" class="bi bi-copy" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M4 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 5a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1v-1h1v1a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h1v1z" />
                     </svg>
             `);
+
+            // Reset color
+            $this.css('color', '');
         }, 2000); // 2-second delay
-
-
-        $('#copy-address').css('color', '#6A8D4D'); // Change icon color to green
-        $('#copy-icon')
 
     }).catch(function (err) {
         // Error callback
         console.error('Failed to copy text: ', err);
     });
 });
+
 
 
 let orderName = '';
